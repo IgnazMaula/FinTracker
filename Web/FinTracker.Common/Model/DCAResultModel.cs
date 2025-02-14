f58e1b10af170231f846427c9b70ba7fc13a09f0
Ignaz Maula Ibrahim <60685073+IgnazMaula@users.noreply.github.com>
2025-02-14 17:35:52 +0700

    Add files via upload

---
+++ Web/FinTracker.Common/Model/DCAResultModel.cs
@@ -0,0 +1,19 @@
+﻿using FinTracker.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinTracker.Common.Model;
+
+public class DCAResultModel
+{
+    public DateTime Date { get; set; }
+    public double Price { get; set; }
+    public double PercentGain { get; set; }
+    public double TotalInvested { get; set; }
+    public double TotalGain { get; set; }
+    public double Total { get; set; }
+}
